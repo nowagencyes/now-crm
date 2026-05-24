11db06d2e5290a75d1ab8cfaa4d6f680b74658ae
nowagencyes <hola@nowagency.es>
2026-05-24 21:25:16 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 
-const SUPABASE_URL = "https://pokrxvtvfrxijcdjrfkv/settings/api-keys";
+const SUPABASE_URL = "https://pokrxvtvfrxjicdjrfkv.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_dvyZXrjf9FhlGyN0rpOz8g_7xVjhX3z";
